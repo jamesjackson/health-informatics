a18179c9c7511b5a8bd87d67e1e5cecd97471ca1
jamesjackson <fiberhog@gmail.com>
2016-01-26 19:24:01 -0600

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
 
+var port = process.env.PORT || 8080;
+
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
@@ -9,6 +11,6 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
