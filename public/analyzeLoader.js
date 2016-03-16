49f6600421a99f3a13de01137125e54c20a99135
Tim <timothy.c.altmansberger@vanderbilt.edu>
2016-03-16 19:53:40 -0400

    Added ability to take or upload photo, shows preview of photo below button

--- public/analyzeLoader.js
+++ public/analyzeLoader.js
@@ -14,3 +14,15 @@ function goSendToDoctor()
 {
     window.location.href = "send.html";
 }
+
+function showPhoto(input)
+{
+	if (input.files && input.files[0])
+	{
+		var reader = new FileReader();
+		reader.onload = function(image) {
+			$('#imageViewer').attr('src', image.target.result).width(150).height(200);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
