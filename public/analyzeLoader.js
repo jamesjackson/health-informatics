a810ac51e00288b216b04c548586b88c9c886622
Tim <timothy.c.altmansberger@vanderbilt.edu>
2016-03-20 13:58:32 -0400

    Added image caching between screens

--- public/analyzeLoader.js
+++ public/analyzeLoader.js
@@ -15,14 +15,21 @@ function goSendToDoctor()
     window.location.href = "send.html";
 }
 
-function showPhoto(input)
+function processImageFile(input)
 {
 	if (input.files && input.files[0])
 	{
 		var reader = new FileReader();
 		reader.onload = function(image) {
-			$('#imageViewer').attr('src', image.target.result).width(150).height(200);
+			$('#imageViewer').attr('src', image.target.result).width(640).height(480);
+			localStorage.setItem("image", image.target.result);
 		}
 		reader.readAsDataURL(input.files[0]);
 	}
+}
+
+function loadImageFromCache(input)
+{
+	var image = localStorage.getItem("image");
+	document.getElementById("imageViewer").src = image; 
 }
--- public/sendLoader.js
+++ public/sendLoader.js
@@ -14,3 +14,22 @@ function goAnalyze()
 {
     window.location.href = "analyze.html";
 }
+
+function processImageFile(input)
+{
+	if (input.files && input.files[0])
+	{
+		var reader = new FileReader();
+		reader.onload = function(image) {
+			$('#imageViewer').attr('src', image.target.result).width(480).height(320);
+			localStorage.setItem("image", image.target.result);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+function loadImageFromCache(input)
+{
+	var image = localStorage.getItem("image");
+	document.getElementById("imageViewer").src = image; 
+}
