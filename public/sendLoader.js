3fbc040d241bdbd92ca58ffcd57d707986bbf17c
Tim <timothy.c.altmansberger@vanderbilt.edu>
2016-04-03 18:53:55 -0400

    Implemented form for getting physicians from server, added some styling to Analyze Condition paper

--- public/sendLoader.js
+++ public/sendLoader.js
@@ -15,6 +15,26 @@ function goAnalyze()
     window.location.href = "analyze.html";
 }
 
+function findPhysician()
+{
+	var firstName = $('#firstName').val();
+	var lastName = $('#lastName').val();	
+	var url = "http://myhealthapp.herokuapp.com/api/practitioner";
+	console.log('fdskafd');
+	// ?first_name="+firstName+"&last_name="+lastName;
+	
+	$.ajax({
+	    url: url,
+	    data: { 
+	        first_name: firstName, 
+	        last_name: lastName
+	    },
+	    //cache: false,
+	    type: "GET",
+	    headers: {'Access-Control-Allow-Origin': '*'}
+	}).done(console.log("fdaf"));
+}
+
 function processImageFile(input)
 {
 	if (input.files && input.files[0])
