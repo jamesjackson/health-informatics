56ecdbe9a4e5eb9c92cd5de1adcfb4f949ed86b8
Robert Delgado <Robert Delgado>
2016-04-03 20:22:58 -0400

    testing

--- public/loginLoader.js
+++ public/loginLoader.js
@@ -17,50 +17,92 @@
     
 localStorage.setItem("currentSize","30");
 
-function attemptLogin()
-{
-	
-    var first_name = document.getElementById("first-name").value;
-    var last_name = document.getElementById("last-name").value;
-    var patient_ID = document.getElementById("patient-id").value;
-	var theUrl = "http://myhealthapp.herokuapp.com/api/" + patient_ID + "/login";
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
+jQuery.ajax = (function(_ajax){
+
+    var protocol = location.protocol,
+        hostname = location.hostname,
+        exRegex = RegExp(protocol + '//' + hostname),
+        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
+        query = 'select * from html where url="{URL}" and xpath="*"';
+
+    function isExternal(url) {
+        return !exRegex.test(url) && /:\/\//.test(url);
     }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            return xmlhttp.responseText;
+
+    return function(o) {
+
+        var url = o.url;
+
+        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
+
+            // Manipulate options so that JSONP-x request is made to YQL
+
+            o.url = YQL;
+            o.dataType = 'json';
+
+            o.data = {
+                q: query.replace(
+                    '{URL}',
+                    url + (o.data ?
+                        (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
+                    : '')
+                ),
+                format: 'xml'
+            };
+
+            // Since it's a JSONP request
+            // complete === success
+            if (!o.success && o.complete) {
+                o.success = o.complete;
+                delete o.complete;
+            }
+
+            o.success = (function(_success){
+                return function(data) {
+
+                    if (_success) {
+                        // Fake XHR callback.
+                        _success.call(this, {
+                            responseText: data.results[0]
+                                // YQL screws with <script>s
+                                // Get rid of them
+                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
+                        }, 'success');
+                    }
+
+                };
+            })(o.success);
+
         }
-    }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();    
-    //alert("http://myhealthapp.herokuapp.com/api/" + patient_ID + "/login");
+
+        return _ajax.apply(this, arguments);
+
+    };
+
+})(jQuery.ajax);
+
+function attemptLogin()
+{   
+    alert("new trial");
     //alert(first_name + " " + last_name + " " + patient_ID);
-   // $.ajax({
-    //url: "http://myhealthapp.herokuapp.com/api/" + patient_ID + "/login",
-    //data: { 
-   //     first_name: first_name, 
-   //     last_name: last_name
-   // },
-   // cache: false,
-   // type: "GET",
-   // headers: {'Access-Control-Allow-Origin': '*'},
-	//error: function (xhr, textStatus, thrownError) {
-	//	alert(textStatus);
-    //    alert(xhr.status);
-    //    alert(xhr.responseText);
-    //  }
-//}).done(function (data) {
- //         if( data.status == 'ok')
-//			window.location.href = "index.html";
-  //    });
+    $.ajax({
+    url: "http://myhealthapp.herokuapp.com/api/" + patient_ID + "/login",
+    data: { 
+        first_name: first_name, 
+        last_name: last_name
+    },
+    cache: false,
+    type: "GET",
+    headers: {'Access-Control-Allow-Origin': '*'},
+	error: function (xhr, textStatus, thrownError) {
+		alert(textStatus);
+        alert(xhr.status);
+        alert(xhr.responseText);
+      }
+}).done(function (data) {
+          if( data.status == 'ok')
+			window.location.href = "index.html";
+      });
 }
 
 //$.get('http://myhealthapp.herokuapp.com/1671251/login', { first_name: 'FREDRICA', last_name: 'SMITH', _sm_au_: 'iMVBMrSljBLFkr37' }, function(data) {
