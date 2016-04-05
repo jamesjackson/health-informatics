bbb132ca6d0b1b496fd34770a6e70a004eedb5f8
Tim <timothy.c.altmansberger@vanderbilt.edu>
2016-04-04 21:48:05 -0400

    Added functionality to fill out symptoms questions on analyze page

--- public/analyzeLoader.js
+++ public/analyzeLoader.js
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 
+function setup()
+{
+	loadImageFromCache();
+	loadSymptomList();
+}
 
 function goIndex()
 {
@@ -13,6 +18,21 @@ function goIndex()
 function goSendToDoctor()
 {
     window.location.href = "send.html";
+}
+
+function loadSymptomList()
+{
+	$.ajax({
+		url: "http://myhealthapp.herokuapp.com/api/symptomslist",
+		dataType:'json',
+		async: false,
+		success: function(data){
+			$('#symptomsQuestions').html(printSymptomsQuestions(data));
+		},
+		error: function(){
+			alert("Could not access server");
+		}
+	});
 }
 
 function processImageFile(input)
@@ -26,6 +46,48 @@ function processImageFile(input)
 		}
 		reader.readAsDataURL(input.files[0]);
 	}
+}
+
+function printSymptomsQuestions(data)
+{
+	var result = "";
+	result += "<form>";
+	for (i = 0 ; i < data.symptoms.length ; i++)
+	{
+		symptom = data.symptoms[i];
+		console.log(symptom);
+		
+		// input entry tag
+		var options = [];
+		if (symptom.type == "boolean")
+		{
+			options = ['Yes', 'No'];
+		} else {
+			tokens = symptom.type.split(':');
+			options = tokens[1].split(',');
+		}
+		console.log(options);
+		// select entry tag
+		result += (symptom.text+"   ");
+		result += "<select>";
+		
+		// add options to select tag
+		for (j = 0 ; j < options.length ; j++)
+		{
+			result += "<option>";
+			result += options[j];
+			result += "</option>";
+		}
+		
+		// select exit tag
+		result += "</select>";
+		
+		// add break
+		result += "<br>";
+	}
+	
+	result += "</form>"
+	return result;
 }
 
 function loadImageFromCache(input)
